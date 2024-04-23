c921baf3fd9a8b1eab8a3d9ad2909edd8d60e4d0
markutopio <101260773+markutopio@users.noreply.github.com>
2024-04-23 19:14:46 +0200

    Add files via upload

---
+++ admin.h
@@ -0,0 +1,12 @@
+#ifndef _ADMIN_
+#define _ADMIN_
+#include "datos.h"
+
+int intro_admin(Adminprov **, int );
+int modifica_admin(Adminprov *,int );
+int alta_admin(Adminprov **,int );
+int baja_admin(Adminprov **,int );
+void listado_admin(Adminprov *,int );
+int busqueda_admin(Adminprov *,int );
+
+#endif // _ADMIN_
