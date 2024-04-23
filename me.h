2f47a4bdf7338f6bfba2d5a1d65584afcb33fee5
markutopio <101260773+markutopio@users.noreply.github.com>
2024-04-23 19:14:23 +0200

    Add files via upload

--- me.h
+++ me.h
@@ -12,4 +12,12 @@ int ventana_admin();
 int ventana_proveedor();
 int ventana_transportista();
 int login(Clientes *,int);
+void iniciar_cliente(Clientes *, int, int);
+void iniciar_admin(Adminprov *, int, int);
+void iniciar_prov(Adminprov *, int, int);
+void iniciar_transp(Transportistas *, int, int);
+void crear_cliente(Clientes *, int *);
+void crear_admin(Adminprov *, int *);
+void crear_prov(Adminprov *, int *);
+void crear_transp(Transportistas *, int *);
 #endif // _ME_
